95d40fc009f99d951552da2397e88ecef75cdc00
GS_song98 <20003204@sju.ac.kr>
2025-04-06 00:28:27 +0900

    refactor: 코드 프로필 이미지 저장 로직 수정
    Co-authored-by: hoyeonyy <highoption@naver.com>

--- src/main/kotlin/codel/member/presentation/MemberController.kt
+++ src/main/kotlin/codel/member/presentation/MemberController.kt
@@ -1,7 +1,9 @@
 package codel.member.presentation
 
 import codel.auth.business.AuthService
+import codel.config.argumentresolver.LoginMember
 import codel.member.business.MemberService
+import codel.member.domain.Member
 import codel.member.presentation.request.CodeImageSavedRequest
 import codel.member.presentation.request.MemberLoginRequest
 import codel.member.presentation.request.ProfileSavedRequest
@@ -39,8 +41,12 @@ class MemberController(
 
     @PostMapping("/v1/member/codeimage")
     override fun saveCodeImage(
+        @LoginMember member: Member,
         @RequestBody request: CodeImageSavedRequest,
     ): ResponseEntity<Unit> {
-        memberService.saveCodeImage(request)
+        // 이미지를 엔티티로 저장
+        // s3 통신 방식 변경 필요
+        memberService.saveCodeImage(member, request)
+        return ResponseEntity.ok().build()
     }
 }
