2cf50dede4e814772a5eebcd237951d671d37a5f
GS_song98 <20003204@sju.ac.kr>
2025-04-07 17:50:18 +0900

    test: 프로필 저장 예외 테스트 추가
    Co-authored-by: hoyeonyy <highoption@naver.com>

--- src/test/kotlin/codel/member/domain/MemberRepositoryTest.kt
+++ src/test/kotlin/codel/member/domain/MemberRepositoryTest.kt
@@ -2,6 +2,7 @@ package codel.member.domain
 
 import codel.config.TestFixture
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
@@ -12,20 +13,7 @@ class MemberRepositoryTest : TestFixture() {
     @DisplayName("프로필 저장 테스트")
     @Test
     fun saveProfileTest() {
-        val seokProfile =
-            Profile(
-                codeName = "seok",
-                age = 28,
-                job = "백엔드 개발자",
-                alcohol = "자주 마심",
-                smoke = "비흡연자 - 흡연자와 교류 NO",
-                hobby = listOf("영화 & 드라마", "여행 & 캠핑"),
-                style = listOf("표현을 잘하는 직진형", "상대가 필요할 때 항상 먼저 연락하는 스타일"),
-                bigCity = "경기도",
-                smallCity = "성남시",
-                mbti = "isfj",
-                introduce = "잘부탁드립니다!",
-            )
+        val seokProfile = createProfile()
 
         memberRepository.saveProfile(hogee, seokProfile)
         val findMember = memberRepository.findMember(hogee.oauthType, hogee.oauthId)
@@ -35,4 +23,54 @@ class MemberRepositoryTest : TestFixture() {
             { assertThat(findMember.memberStatus).isEqualTo(MemberStatus.CODE_SURVEY) },
         )
     }
+
+    @DisplayName("멤버 아이디에 해당하는 멤버가 없으면 예외를 반환한다.")
+    @Test
+    fun saveProfileWithoutMemberTest() {
+        val seokMember = createMember()
+        val seokProfile = createProfile()
+
+        assertThatThrownBy { memberRepository.saveProfile(seokMember, seokProfile) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("멤버가 존재하지 않습니다.")
+    }
+
+    @DisplayName("바꾸고자 하는 멤버 아이디가 없으면 예외를 반환한다.")
+    @Test
+    fun changeMemberStatusWithoutMemberIdTest() {
+        val seokMember = createMemberWithoutMemberId()
+        val seokProfile = createProfile()
+
+        assertThatThrownBy { memberRepository.saveProfile(seokMember, seokProfile) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("member id가 비어있습니다.")
+    }
+
+    private fun createProfile(): Profile =
+        Profile(
+            codeName = "seok",
+            age = 28,
+            job = "백엔드 개발자",
+            alcohol = "자주 마심",
+            smoke = "비흡연자 - 흡연자와 교류 NO",
+            hobby = listOf("영화 & 드라마", "여행 & 캠핑"),
+            style = listOf("표현을 잘하는 직진형", "상대가 필요할 때 항상 먼저 연락하는 스타일"),
+            bigCity = "경기도",
+            smallCity = "성남시",
+            mbti = "isfj",
+            introduce = "잘부탁드립니다!",
+        )
+
+    private fun createMember(): Member =
+        Member(
+            id = 100L,
+            oauthType = OauthType.APPLE,
+            oauthId = "seok",
+        )
+
+    private fun createMemberWithoutMemberId(): Member =
+        Member(
+            oauthType = OauthType.APPLE,
+            oauthId = "seok",
+        )
 }
