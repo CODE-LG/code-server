eb469e01524152b201befa3e3e2d4f1dec54c050
hoyeonyy <highoption@naver.com>
2025-04-07 20:04:10 +0900

    test: MemberArgumentResolverTest 생성
    Co-authored-by: SongGwanSeok <20003204@sju.ac.kr>

--- src/main/kotlin/codel/config/argumentresolver/MemberArgumentResolver.kt
+++ src/main/kotlin/codel/config/argumentresolver/MemberArgumentResolver.kt
@@ -6,6 +6,7 @@ import codel.member.domain.OauthType
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
@@ -22,15 +23,15 @@ class MemberArgumentResolver(
         parameter: MethodParameter,
         mavContainer: ModelAndViewContainer?,
         webRequest: NativeWebRequest,
-        binderFactory: org.springframework.web.bind.support.WebDataBinderFactory?,
+        binderFactory: WebDataBinderFactory?,
     ): Any? {
         val httpServletRequest =
             webRequest.getNativeRequest(HttpServletRequest::class.java)
                 ?: throw IllegalStateException("HttpServletRequest를 가져올 수 없습니다.")
+
         val oauthId =
             httpServletRequest.getAttribute("oauthId") as? String
                 ?: throw IllegalArgumentException("oauthId가 요청이 없습니다.")
-
         val oauthType =
             httpServletRequest.getAttribute("oauthType") as? OauthType
                 ?: throw IllegalArgumentException("oauthType이 요청이 없습니다.")
